5102879c7463579f94344078333609dee1225187
David Alasow <davidalasow@gmail.com>
2016-06-20 14:04:05 +0000

    Changed some commands and added some new ones

--- plugins/random.js
+++ plugins/random.js
@@ -11,3 +11,33 @@ commands.on('invite', function roll(user, userID, channelID, message, args) {
         message: `${bot.inviteURL}`
     });
 });
+
+commands.on('clear', function roll(user, userID, channelID, message, args) {
+    if (userID !== db.settings.owner) return;
+    if (typeof(args._[0]) !== 'number') return;
+    bot.getMessages({
+        channel:channelID,
+        limit: 100
+    }, function(error, messages) {
+        var botMessages = [];
+        if (error) return console.log(error);
+        for (var i=0; i < messages.length; i++) {
+            if (messages[i].author.username !== bot.username) continue;
+            botMessages.push(messages[i]);
+            //bot.deleteMessage()
+        }
+        for (var i=0; i < args._[0]; i++) {
+            bot.deleteMessage({
+                channel: channelID,
+                messageID: botMessages[i].id
+            });
+        }
+    });
+});
+
+commands.on('lastmention', function roll(user, userID, channelID, message, args) {
+    var messages = bot.getMessages({
+        channel: channelID,
+        limit: 5000
+    });
+});
